2dbf7ca94be27c2b58809c0ee47d0d7dc4389905
pivnati <natanmuleta911@gmail.com>
2025-05-02 10:34:27 -0700

    create the intil in the pubspec

--- flu_ap/lib/ui/chat_screen.dart
+++ flu_ap/lib/ui/chat_screen.dart
@@ -12,50 +12,53 @@ class _ChatScreenState extends State<ChatScreen> {
   Color black = Color(0xFF191919);
 
   TextEditingController msgInputController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: black,
-      body: Container(
+      body: SafeArea(
         child: Column(
           children: [
-            Expanded(flex: 9, child: Container(
-              child: Container(
-                child: ListView.builder(itemBuilder:(context,index){
-                  return MessageItem();
-                }),
-              ),
-            )),
+            // Message list
             Expanded(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  style: TextStyle(color: Colors.white),
-                  cursorColor: purple,
-                  controller: msgInputController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+              flex: 9,
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return MessageItem(sentByMe: index % 2 == 0); // alternating messages
+                },
+              ),
+            ),
+            // Input field
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                cursorColor: purple,
+                controller: msgInputController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  suffixIcon: Container(
+                    margin: EdgeInsets.only(right: 10),
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: purple,
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    suffixIcon: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: purple,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          sendMessage(msgInputController.text);
-                          msgInputController.clear();
-                        },
-                        icon: Icon(Icons.send, color: Colors.white),
-                      ),
+                    child: IconButton(
+                      onPressed: () {
+                        sendMessage(msgInputController.text);
+                        msgInputController.clear();
+                      },
+                      icon: Icon(Icons.send, color: Colors.white),
                     ),
                   ),
                 ),
@@ -66,14 +69,34 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
     );
   }
-  
-  void sendMessage(String text) {}
+
+  void sendMessage(String text) {
+    // Placeholder for sending messages
+    print('Sending: $text');
+  }
 }
+
 class MessageItem extends StatelessWidget {
-  const MessageItem({super.key});
+  final bool sentByMe;
+
+  const MessageItem({super.key, required this.sentByMe});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      alignment: sentByMe ? Alignment.centerRight : Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: sentByMe ? Colors.purple : Colors.grey[800],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          sentByMe ? "Hello from me!" : "Hello from them!",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
