ea84cc2851e119510bf458603c2fb72aecf06f7c
pivnati <natanmuleta911@gmail.com>
2025-05-02 09:23:11 -0700

    create the chat screen .dart file

---
+++ flu_ap/lib/ui/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Color purple = Color(0xFF6c5ce7);
+  Color black = Color(0xFF191919);
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: black,
+      body: Container(
+        
+      ),
+    );
+  }
+}
