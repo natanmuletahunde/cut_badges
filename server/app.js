37e107fcf1bc18ea6b0f31c5d985190c06cc26b5
pivnati <natanmuleta911@gmail.com>
2025-05-02 16:59:35 -0700

    finshe the app

--- server/app.js
+++ server/app.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const socketIO = require('socket.io');
-const cors = require('cors');  // Add this line
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Enable CORS
-app.use(cors());  // Use cors middleware
+app.use(cors());
 
 const server = app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
 });
 
-const io = socketIO(server);
-
+const io =require('socket.io')(server)
+ const connectedUser = new Set();
 io.on('connection', (socket) => {
   console.log('Client connected:', socket.id);
-  socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
-  });
+  io.emit('connected-user', connectedUser.size) // Send a welcome message to the connected client
+  connectedUser.add(socket.id)
 
   socket.on('message', (data) => {
-    console.log('Message received:', data); // should now log JSON from Flutter
-    io.emit('message', data); // broadcast to all clients
-});
+    console.log(JSON.stringify(data, null, 2));
+    socket.broadcast.emit('message-receive' , data) // Pretty print the message
+  });
 
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+    io.emit('connected-user', connectedUser.size) // Send a welcome message to the connected client
+
+  });
 });
