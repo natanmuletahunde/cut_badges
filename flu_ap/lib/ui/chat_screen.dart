ec8d792769713724193473fa33a59887ca7342fa
pivnati <natanmuleta911@gmail.com>
2025-05-02 10:22:18 -0700

    create the stateless widget in the chat_screen.dart

--- flu_ap/lib/ui/chat_screen.dart
+++ flu_ap/lib/ui/chat_screen.dart
@@ -14,44 +14,66 @@ class _ChatScreenState extends State<ChatScreen> {
   TextEditingController msgInputController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: black,
       body: Container(
         child: Column(
-        
           children: [
-            Expanded(flex:9,child:Container()),
-            Expanded(child: 
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                style:TextStyle(
-                  color: Colors.white
-                ),
-                cursorColor: purple,
-                controller:msgInputController,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  suffixIcon: Container(
-                    child: IconButton(onPressed: () {}, icon: Icon(Icons.send)),
-                  )
-                ),
-
+            Expanded(flex: 9, child: Container(
+              child: Container(
+                child: ListView.builder(itemBuilder:(context,index){
+                  return MessageItem();
+                }),
               ),
-            )
-            
-            )
+            )),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  cursorColor: purple,
+                  controller: msgInputController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    suffixIcon: Container(
+                      margin: EdgeInsets.only(right: 10),
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: purple,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          sendMessage(msgInputController.text);
+                          msgInputController.clear();
+                        },
+                        icon: Icon(Icons.send, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
-
       ),
     );
+  }
+  
+  void sendMessage(String text) {}
+}
+class MessageItem extends StatelessWidget {
+  const MessageItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
