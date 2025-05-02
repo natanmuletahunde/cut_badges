78896875a211343e8255fdfe6239ad686417b098
pivnati <natanmuleta911@gmail.com>
2025-05-02 09:41:53 -0700

    commit the chat screen .dart

--- flu_ap/lib/ui/chat_screen.dart
+++ flu_ap/lib/ui/chat_screen.dart
@@ -10,12 +10,47 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   Color purple = Color(0xFF6c5ce7);
   Color black = Color(0xFF191919);
+
+  TextEditingController msgInputController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: black,
       body: Container(
+        child: Column(
         
+          children: [
+            Expanded(flex:9,child:Container()),
+            Expanded(child: 
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                style:TextStyle(
+                  color: Colors.white
+                ),
+                cursorColor: purple,
+                controller:msgInputController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  suffixIcon: Container(
+                    child: IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+                  )
+                ),
+
+              ),
+            )
+            
+            )
+          ],
+        ),
+
       ),
     );
   }
